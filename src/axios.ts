720f463d97e3e20c1bf85a45b773c6b5d94711bc
AlexLen <lenchenkov.a.d@gmail.com>
2023-05-28 17:37:33 +0500

    changed url

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:4444/api',
+	baseURL: 'https://kvantorium-backend.onrender.com/api',
 });
 
 instance.interceptors.request.use((config) => {
