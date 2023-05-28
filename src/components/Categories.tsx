2ddb3e3ab4cad48bf7587b2cd572a28de7816cec
AlexLen <lenchenkov.a.d@gmail.com>
2023-05-28 17:43:42 +0500

    changed url for categories

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Category } from '../redux/category/types';
+import { mainURL } from '../axios';
 
 type CategoriesProps = {
 	items: Category[];
@@ -13,11 +14,7 @@ const Categories: React.FC<CategoriesProps> = ({ items }) => {
 					key={obj._id}
 					className="hover:bg-gray-200 px-2  text-gray-600 hover:text-blue-500 rounded-md py-4 cursor-pointer transition-all duration-300">
 					<div className="flex items-center ">
-						<img
-							className="w-6 h-6 mr-3"
-							src={`http://localhost:4444${obj.imageUrl}`}
-							alt="kvantum"
-						/>
+						<img className="w-6 h-6 mr-3" src={`${mainURL}${obj.imageUrl}`} alt="kvantum" />
 						{obj.name}
 					</div>
 				</li>
