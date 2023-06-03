36b2746b084fb9d815c1fd6d3c0ee1e6ea1bec48
AlexLen <lenchenkov.a.d@gmail.com>
2023-06-03 07:24:53 +0500

    chanded mainURL

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// export const mainURL = 'https://kvantorium-backend.onrender.com';
-export const mainURL = 'http://localhost:4444';
+export const mainURL = 'https://kvantorium-backend.onrender.com';
+// export const mainURL = 'http://localhost:4444';
 
 const instance = axios.create({
 	baseURL: `${mainURL}/api`,
